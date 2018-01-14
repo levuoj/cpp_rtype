17a99ed7543bacb31e2fb097776e66ec4349c324
Anthony Jouvel <anthony.jouvel@epitech.eu>
2018-01-14 16:45:52 +0100

    ADD server udp to main

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -1,11 +1,22 @@
-#include <external_things/Test/Test.hpp>
 #include <utils/Loader.hpp>
+#include <boost/asio/io_service.hpp>
+#include "server/UdpServer.hpp"
 
 int main() {
-    Loader<Test>            loader("./lib/libTest.so");
+    Mediator mediator;
+    /*Loader<Test> loader("./lib/libTest.so");
 
     loader.Open();
     loader.Load("entryPoint");
-    loader.getInstance()->helloWorld();
-    loader.Close();
+    loader.getInstance()->helloWorld();*/
+    try
+    {
+        boost::asio::io_service io_service;
+        Server::UdpServer server(mediator, io_service);
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
