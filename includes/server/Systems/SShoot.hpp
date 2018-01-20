a38440c93349f575c7933baed3f4a6066cbc18bc
Kilian <kilian.lebrun@epitech.eu>
2018-01-20 17:38:47 +0100

    FIX SShoot

--- includes/server/Systems/SShoot.hpp
+++ includes/server/Systems/SShoot.hpp
@@ -5,10 +5,22 @@
 #ifndef CPP_RTYPE_SSHOOT_HPP
 #define CPP_RTYPE_SSHOOT_HPP
 
+#include <server/ASystem.hpp>
+#include <utility>
 
-class SShoot {
+namespace FF
+{
+    class SShoot : public ASystem
+    {
+        std::function<void(EEntityType, EElement)>   _callback;
+    public:
+        SShoot();
+        ~SShoot() final = default;
 
-};
+        void    execute() final;
+        void    setCallback(std::function<void(EEntityType, EElement)> func) { _callback = std::move(func); }
+    };
+}
 
 
 #endif //CPP_RTYPE_SSHOOT_HPP
--- sources/server/Systems/SShoot.cpp
+++ sources/server/Systems/SShoot.cpp
@@ -2,4 +2,22 @@
 // Created by Oraekia on 17/01/18.
 //
 
-#include "SShoot.hpp"
+#include <server/APlayer.hpp>
+#include <iostream>
+#include "server/Systems/SShoot.hpp"
+
+FF::SShoot::SShoot() : ASystem("Shoot") {}
+
+void FF::SShoot::execute()
+{
+    for (const auto &itET : _entities)
+    {
+        if (itET.second->getType() == EEntityType::PLAYER)
+        {
+            CPosition pos = *reinterpret_cast<APlayer *>(itET.second.get())->getPosition();
+            pos.setX(pos.getX() + 0.5f);
+            if (this->getMap()->doShifting(itET.second.get()->getId(), EElement::PLAYERMISSILE, pos))
+                _callback(EEntityType::PLAYERMISSILE, EElement::PLAYERMISSILE);
+        }
+    }
+}
